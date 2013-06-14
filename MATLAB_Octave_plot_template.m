81e2fa5b66c280e6a652004e137e82efe4136548
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2013-06-14 12:54:57 -0500

    Updated MATLAB/Octave plot template
    The template now reflects differing options and look of plots between
    MATLAB and Octave.

--- MATLAB_Octave_plot_template.m
+++ MATLAB_Octave_plot_template.m
@@ -15,7 +15,8 @@
 % Created: 4/19/13 - Joshua Vaughan (joshua.vaughan@louisiana.edu)
 %
 % Modified: 
-%   * 
+%   * 6/14/13 - Joshua Vaughan - joshua.vaughan@louisiana.edu
+%       - now changes options based on MATLAB or Octave
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -31,10 +32,26 @@ y3 = 0.75*sin(x3);
 y4 = 1.25*sin(x4);
 
 
+
 %-----  Copy from here down into your code, replacing items as needed ----------------
 %
+% Decide if user is using MATLAB or Octave
+% This will return a nonzero value if in Octave
+isOctave = exist('OCTAVE_VERSION');
+
+if isOctave == 0 % We're in MATLAB
+    plot_line_width = 3;
+    grid_line_width = 2;
+else % We're in Octave
+    plot_line_width = 8;
+    grid_line_width = 4;
+end
+
+% Set consistent font
+font_name = 'Times New Roman';
+
 %  The plot is formatted for saving, so it might be ugly on screen
-h = figure
+h = figure;
 
 % Define the papersize and margins
 set (h,'papertype', '<custom>')
@@ -48,27 +65,43 @@ set (h,'paperposition', [0,0,[9 6]])
 set (gca,'position', [0.19, 0.19, 0.75, 0.75]) 
 
 % Increase the axis font size to somethign readable
-set (gca, "fontsize", 24)
+set (gca, 'fontsize', 24)
 
-% Change all fonts to CMU Serif
-FN = findall(h,'-property','FontName');
-set(FN,'FontName','CMU Serif');
-
-% This is the actual plotting... Change xi and yi
-plot(x1,y1,'LineWidth',6,x2,y2,'--','LineWidth',6,x3,y3,'-.','LineWidth',6,x4,y4,':','LineWidth',6)
+% The options are only specified once in MATLAB. Octave let's you adjust
+% each line. This means we need to define the LineWidth for each variable
+% in Octave.
+if isOctave == 0 % We're in MATLAB
+    % This is the actual plotting... Change xi and yi
+    plot(x1,y1,x2,y2,'--',x3,y3,'-.',x4,y4,':','LineWidth',plot_line_width)
+else % We're in Octave 
+    % This is the actual plotting... Change xi and yi
+    plot(x1,y1,'LineWidth',plot_line_width,x2,y2,'--','LineWidth',plot_line_width,x3,y3,'-.','LineWidth',plot_line_width,x4,y4,':','LineWidth',plot_line_width)
+end
 
 % Show a grid and set an appropriate line thickness 
 grid on
-set (gca, 'LineWidth',4)
+set (gca, 'LineWidth',grid_line_width)
 
 % Set the X and Y axis labels - Change these to suit your plot
 xlabel('X Label (units)','fontsize',28)
 ylabel('Y Label (units)','fontsize',28)
 
 % Create the legend - change 'Data i' to suit your plot
-leg = legend('Data 1','Data 2','Data 3','Data 4')
-set (leg, "FontSize", 20,'FontName','CMU Serif') 
+leg = legend('Data 1','Data 2','Data 3','Data 4');
+set (leg, 'FontSize', 20,'FontName',font_name) 
 
-% Save the figure as a high-res pdf
-print(h,'plot_filename.pdf','-dpdf','-color','-landscape')
+% Change all fonts to font_name
+FN = findall(h,'-property','FontName');
+set(FN,'FontName',font_name);
+
+
+% check if MATLAB or Octave to descide how to save
+if isOctave == 0; % this is MATLAB
+    % Save the figure as a high-res pdf
+    print(h,'plot_filename.pdf','-dpdf');
+else
+    % Save the figure as a high-res pdf
+    print(h,'plot_filename.pdf','-dpdf','-color','-landscape')
+end
+
 
