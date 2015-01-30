f1694c6e97ed990e48ac5ca9609cf3534c01372d
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2015-01-30 01:59:09 -0600

    Update to Flask D-pad center's CSS
    * Added blending mode in the CSS
    * Adjusted the spacing for better alignment
    
    * Also added a matplotlib snippet

---
+++ Plotting/matplotlib_snippet.py
@@ -0,0 +1,45 @@
+# Set the plot size - 3x2 aspect ratio is best
+fig = plt.figure(figsize=(6,4))
+ax = plt.gca()
+plt.subplots_adjust(bottom=0.17, left=0.17, top=0.96, right=0.96)
+
+# Change the axis units to CMUSerif-Roman
+plt.setp(ax.get_ymajorticklabels(),fontsize=18)
+plt.setp(ax.get_xmajorticklabels(),fontsize=18)
+
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+# Turn on the plot grid and set appropriate linestyle and color
+ax.grid(True,linestyle=':', color='0.75')
+ax.set_axisbelow(True)
+
+# Define the X and Y axis labels
+plt.xlabel('X label (units)', fontsize=22, weight='bold', labelpad=5)
+plt.ylabel('Y label (units)', fontsize=22, weight='bold', labelpad=10)
+ 
+plt.plot(x1,y1, linewidth=2, linestyle="-", label=r'Data 1')
+plt.plot(x2,y2, linewidth=2, linestyle="--", label=r'Data 2')
+plt.plot(x3,y3, linewidth=2, linestyle="-.", label=r'Data 3')
+plt.plot(x4,y4, linewidth=2, linestyle=":", label=r'Data 4')
+
+# uncomment below and set limits if needed
+# plt.xlim(0,5)
+# plt.ylim(0,10)
+
+# Create the legend, then fix the fontsize
+leg = plt.legend(loc='upper right', ncol = 1, fancybox=True)
+ltext  = leg.get_texts()
+plt.setp(ltext,fontsize=18)
+
+# Adjust the page layout filling the page using the new tight_layout command
+plt.tight_layout(pad=0.5)
+
+# save the figure as a high-res pdf in the current folder
+# plt.savefig('plot_filename.pdf')
+
+# show the figure
+show()
