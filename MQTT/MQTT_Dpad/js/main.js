c90bab20c7c1b1974848f399684e2856c9a78d36
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2015-02-16 14:28:16 -0600

    Adapting javascript draggable joystick to MQTT

--- MQTT/MQTT_Dpad/js/main.js
+++ MQTT/MQTT_Dpad/js/main.js
@@ -1,7 +1,30 @@
 // SocketIO functions when the document is ready
 $(document).ready(function(){
-    // Read and send the Dpad button states over a websocket
-    var dpad = "0,0";
+
+    // Functions for the draggable joystick - implemented in Joystick.js
+    vec = Object.seal({
+    x: 0,
+    y: 0
+    });
+
+//     tester = $('#tester');
+//     tester.draggable();
+
+    JoyStick('#joystick1', 120, function(magnitude, theta, ximpulse, yimpulse) {
+        //console.log(magnitude, theta, ximpulse, yimpulse);
+        vec.x = 10 * (ximpulse / 80);
+        vec.y = 10 * (yimpulse / 80);
+    });
+
+//     iid = setInterval(function() {
+//         console.log(vec.x, vec.y);
+// //         tester.css('top', (parseInt(tester.css('top').substr(0, tester.css('top').length - 2), 10) - vec.y) + 'px');
+// //         tester.css('left', (parseInt(tester.css('left').substr(0, tester.css('left').length - 2), 10) + vec.x) + 'px');
+//     }, 50);
+
+
+
+    // Read and send the Dpad button states over MQTT
     var timeout_up = $('#up');
     var timeout_down = $('#down');
     var timeout_right = $('#right');
@@ -27,146 +50,165 @@ $(document).ready(function(){
 
     client.connect(options);
 
-     //Gets  called if the websocket/mqtt connection gets disconnected for any reason
-     client.onConnectionLost = function (responseObject) {
-         //Depending on your scenario you could implement a reconnect logic here
-         alert("connection lost: " + responseObject.errorMessage);
-         $('.connected').toggleClass('on');
-     };
+    //Gets  called if the websocket/mqtt connection gets disconnected for any reason
+    client.onConnectionLost = function (responseObject) {
+        //Depending on your scenario you could implement a reconnect logic here
+        alert("connection lost: " + responseObject.errorMessage);
+        $('.connected').toggleClass('on');
+    };
 
-     //Gets called whenever you receive a message for your subscriptions
-     client.onMessageArrived = function (message) {
-         //Do something with the push message you received
-         $('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
-     };
+    //Gets called whenever you receive a message for your subscriptions
+    client.onMessageArrived = function (message) {
+        //Do something with the push message you received
+        $('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
+    };
 
-     //Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
-     publish = function (payload, topic, qos) {
-         //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
-         var message = new Messaging.Message(payload);
-         message.destinationName = topic;
-         message.qos = qos;
-         client.send(message);
+    //Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
+    publish = function (payload, topic, qos) {
+        //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
+        var message = new Messaging.Message(payload);
+        message.destinationName = topic;
+        message.qos = qos;
+        client.send(message);
     }
     
-    
+    // Up Button
     $('#up').mousedown(function () {
+        vec.x = 0;
+        vec.y = 100;
         dpad = "0,100";
-//         timeout_up = setInterval(function () { 
-//         }, 100);
-
         return false;
     });
+    
     $('#up').mouseup(function () {
-        dpad = "0,0";
-//         clearInterval(timeout_up);
+        vec.x = 0;
+        vec.y = 0;
         return false;
-
     });
+    
     $('#up').mouseout(function () {
-        dpad = "0,0";
-//         clearInterval(timeout_up);
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
     
     $('#up').on("touchstart", function () {
-        dpad = "0,100";
+        vec.x = 0;
+        vec.y = 100;
         return false;
     });
     
     $('#up').on("touchend", function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
     
 
     // right Button    
     $('#right').mousedown(function () {
-        dpad = "100,0";
+        vec.x = 100;
+        vec.y = 0;
         timeout_right = setInterval(function () { 
         }, 100);
 
         return false;
     });
+    
     $('#right').mouseup(function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         clearInterval(timeout_right);
         return false;
     });
+    
     $('#right').mouseout(function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         clearInterval(timeout_right);
         return false;
     });
     
     $('#right').on("touchstart", function () {
-        dpad = "100,0";
+        vec.x = 100;
+        vec.y = 0;
         return false;
     });
     
     $('#right').on("touchend", function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
     
     
     // Down Button    
     $('#down').mousedown(function () {
-        dpad = "0,-100";
-        timeout_down = setInterval(function () { 
-        }, 100);
-
+        vec.x = 0;
+        vec.y = -100;
         return false;
     });
+    
     $('#down').mouseup(function () {
-        dpad = "0,0";
-        clearInterval(timeout_down);
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
+    
     $('#down').mouseout(function () {
-        dpad = "0,0";
-        clearInterval(timeout_down);
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
     
     $('#down').on("touchstart", function () {
-        dpad = "0,-100";
+        vec.x = 0;
+        vec.y = -100;
         return false;
     });
     
     $('#down').on("touchend", function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
     
+    
     // Left Button    
     $('#left').mousedown(function () {
-        dpad = "-100,0";
+        vec.x = -100;
+        vec.y = 0;
         timeout_left = setInterval(function () { 
         }, 100);
-
         return false;
     });
+    
     $('#left').mouseup(function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         clearInterval(timeout_left);
         return false;
     });
+    
     $('#left').mouseout(function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         clearInterval(timeout_left);
         return false;
     });
     
     $('#left').on("touchstart", function () {
-        dpad = "-100,0";
+        vec.x = -100;
+        vec.y = 0;
         return false;
     });
     
     $('#left').on("touchend", function () {
-        dpad = "0,0";
+        vec.x = 0;
+        vec.y = 0;
         return false;
     });
+
 
 // Send the data over a MQTT 10 times per second (100ms)
 toggle_sending = function () {
@@ -190,7 +232,7 @@ toggle_sending = function () {
     }
 send_data = function (){
 //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
-         var message = new Messaging.Message(String(dpad));
+         var message = new Messaging.Message(String(vec.x) + ',' + String(vec.y));
          message.destinationName = 'CRAWLAB';
          message.qos = 0;
          client.send(message);
