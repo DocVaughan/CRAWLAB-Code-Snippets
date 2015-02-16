1d28848bfb622e0934827cce250c106e249d0a1a
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2015-02-15 21:14:34 -0600

    Better "Start" button positioning

---
+++ MQTT/MQTT_ThreadedSub.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+
+##########################################################################################
+# MQTT_ThreadedSub.py
+#
+# Basic implementation of a threaded MQTT client
+#
+# From: http://eclipse.org/paho/clients/python/
+#
+# NOTE: Any plotting is set up for output, not viewing on screen.
+#       So, it will likely be ugly on screen. The saved PDFs should look
+#       better.
+#
+# Created: 01/14/15
+#   - Joshua Vaughan
+#   - joshua.vaughan@louisiana.edu
+#   - http://www.ucs.louisiana.edu/~jev9637
+#
+# Modified:
+#   *
+#
+##########################################################################################
+
+import paho.mqtt.client as mqtt
+import time
+
+ 
+# ## Eclipse
+# HOST = 'iot.eclipse.org'
+# PORT = 1883
+# USERNAME = None
+# PASSWORD = None
+
+# MQTT Dashboard
+HOST = 'broker.mqttdashboard.com'
+PORT = 1883
+USERNAME = None
+PASSWORD = None
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("CRAWLAB")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    # print(msg.topic + ' Received at: ' + str(time.time()) + ' Sent at: ' + str(msg.payload) + ' Latency: {:0.4f}'.format(time.time() - float(msg.payload)))
+    print('Received Topic: ' + msg.topic + '\t\t Data: ' + msg.payload)
+
+client = mqtt.Client()
+
+if USERNAME is not None:
+    client.username_pw_set(USERNAME, PASSWORD)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(HOST, PORT, 60)
+
+# Non-blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+client.loop_start()
+
+# This loop will run printing the kernel time while also reading MQTT data
+try: 
+    while True:
+        print time.time()
+        time.sleep(1)
+except (KeyboardInterrupt, SystemExit):
+    client.loop_stop()
