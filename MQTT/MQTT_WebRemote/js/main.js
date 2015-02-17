b31e76eb4502503869cf4198c5e488c6cf1292f7
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2015-02-17 01:46:49 -0600

    Renamed MQTT WebRemote - fixed touch
    * Had to change the order of library calls
    * touch library had to come after jquery and jquery-UI
    * Now supports touch on the joystick controller
    * Added links to Dpad and Joystick pages

--- MQTT/MQTT_WebRemote/js/main.js
+++ MQTT/MQTT_WebRemote/js/main.js
@@ -1,6 +1,5 @@
 // SocketIO functions when the document is ready
 $(document).ready(function(){
-
     // Functions for the draggable joystick - implemented in Joystick.js
     vec = Object.seal({
     x: 0,
@@ -246,6 +245,34 @@ send_data = function (){
         }
 });
 
+$(function navigation() {
+// 	   
+//       // Create the dropdown base
+//       $("<select />").appendTo("nav");
+//       
+//       // Create default option "Go to..."
+//       $("<option />", {
+//          "selected": "selected",
+//          "value"   : "",
+//          "text"    : "Go to..."
+//       }).appendTo("nav select");
+//       
+//       // Populate dropdown with menu items
+//       $("nav a").each(function() {
+//        var el = $(this);
+//        $("<option />", {
+//            "value"   : el.attr("href"),
+//            "text"    : el.text()
+//        }).appendTo("nav select");
+//       });
+//       
+	   // To make dropdown actually work
+	   // To make more unobtrusive: http://css-tricks.com/4064-unobtrusive-page-changer/
+      $("nav select").change(function() {
+        window.location = $(this).find("option:selected").val();
+      });
+	 
+	 });
 
 
 
