eaef69262418d11ae301d28b6331fe7ccdc006b8
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2015-07-27 13:13:06 -0500

    Update to keyboard reading header

--- Misc Python Tools and Helpers/reading_keyboard_input.py
+++ Misc Python Tools and Helpers/reading_keyboard_input.py
@@ -1,53 +1,26 @@
 #!/usr/bin/env python
 
-###############################################################
-#
-# talker.py
-#
-# This program uses ROSS to send a command to a listener node which will move a dynamixel servo.
-#
-# Created 21 July, 2015
-#   -Matthew Begneaud
-#
-# References:
-# -This program borrows formatting from the tutorials found at: wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
-#
-#Below is their required license and Copyright information:
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
+#! /usr/bin/env python
 
-###############################################################
+##########################################################################################
+# reading_keyboard_input.py
+#
+# Reads keyboard input using either the readchar or pygame modules
+# Just prints the key pressed if it matches a choice
+#
+# NOTE: Any plotting is set up for output, not viewing on screen.
+#       So, it will likely be ugly on screen. The saved PDFs should look
+#       better.
+#
+# Created: 07/27/15
+#   - Joshua Vaughan
+#   - joshua.vaughan@louisiana.edu
+#   - http://www.ucs.louisiana.edu/~jev9637
+#
+# Modified:
+#   *
+#
+##########################################################################################
 
 from __future__ import print_function
 
@@ -62,7 +35,7 @@ clock = pygame.time.Clock()
 
 USE_READCHAR = False     # True to use readchar, false to use pygame
 
-def talker():
+def read_keys():
     SHUTDOWN = False
     
     while not SHUTDOWN:
@@ -128,7 +101,7 @@ def talker():
 
 if __name__ == '__main__':
     try:
-        talker()
+        read_keys()
 
     except (KeyboardInterrupt, SystemExit):
         print('\nExiting...')
