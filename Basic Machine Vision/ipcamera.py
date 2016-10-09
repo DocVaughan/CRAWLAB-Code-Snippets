bd179f77b78caf11a4fed7a8df2ce9810b51cffb
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2016-10-09 14:47:33 -0500

    IP camera initial script - NOT working

---
+++ Basic Machine Vision/ipcamera.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+
+###############################################################################
+# ipcamera.py
+#
+# classes for handling IP cameras in openCV
+#
+# NOTE: Any plotting is set up for output, not viewing on screen.
+#       So, it will likely be ugly on screen. The saved PDFs should look
+#       better.
+#
+# Created: 10/03/16
+#   - Joshua Vaughan
+#   - joshua.vaughan@louisiana.edu
+#   - http://www.ucs.louisiana.edu/~jev9637
+#
+# Modified:
+#   * 
+#
+# TODO:
+#   * 
+###############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import base64
+import time
+import urllib.request
+
+import cv2
+import numpy as np
+
+
+"""
+Examples of objects for image frame aquisition from both IP and
+physically connected cameras
+Requires:
+ - opencv (cv2 bindings)
+ - numpy
+"""
+
+
+class ipCamera(object):
+
+    def __init__(self, url, user=None, password=None):
+        self.url = url
+        auth_encoded = base64.encodestring('%s:%s' % (user, password))[:-1]
+
+        self.req = urlib.request.Request(self.url)
+        self.req.add_header('Authorization', 'Basic %s' % auth_encoded)
+
+    def get_frame(self):
+        response = urlib.request.urlopen(self.req)
+        img_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
+        frame = cv2.imdecode(img_array, 1)
+        return frame
+
+
+class Camera(object):
+
+    def __init__(self, camera=0):
+        self.cam = cv2.VideoCapture(camera)
+        if not self.cam:
+            raise Exception("Camera not accessible")
+
+        self.shape = self.get_frame().shape
+
+    def get_frame(self):
+        _, frame = self.cam.read()
+        return frame
+        
+if __name__ == "__main__":
+    IP_ADDRESS = '10.50.61.62'
+    USER = 'guest'
+    PASSWORD = 'viewandmove'
+
+    ip_cam = ipCamera(IP_ADDRESS, USER, PASSWORD)
+    
+    imshow(ip_cam.get_frame)
