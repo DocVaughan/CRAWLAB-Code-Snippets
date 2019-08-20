25c71818d11a0873f74267679f92c189798038df
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2019-08-20 17:10:41 -0400

    Update to Gekko MPC, began work on nonlinear sys with Gekko

--- Model Predictive Control/mpc_nonlinear_mass_spring_damper_Gekko.py
+++ Model Predictive Control/mpc_nonlinear_mass_spring_damper_Gekko.py
@@ -28,24 +28,60 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from tqdm import tqdm
+import time
 
 from gekko import GEKKO
 
+
+def eq_of_motion_nonlinear(t, w, p):
+    """
+    Defines the differential equations for the nonlinear spring-mass system.
+    It's has a hardening spring and hardening damper.
+
+    Arguments:
+        w :  vector of the state variables:
+        t :  time
+        p : array-like parameter list
+    """
+    
+    x = w[0]
+    x_dot = w[1]
+    y = w[2]
+    y_dot = w[3]
+    
+    m1, m2, k, c, u = p
+    
+    #k = 1.25*k
+
+    # Create sysODE = (x', x_dot', y', y_dot')
+    sysODE = np.array([x_dot,
+                       k/m1 * (y - x) + c/m1 * (y_dot - x_dot) + u/m1,
+                       y_dot,
+                       -k/m2 * (y - x) - c/m2 * (y_dot - x_dot)])
+    
+    return sysODE
+
+
 m = GEKKO()
-m.time = np.linspace(0, 5, 501)
+m.time = np.linspace(0, 0.5, 51)
 
 # m.options.CTRL_HOR = 100
 # m.options.PRED_HOR = 200
-# m.options.SOLVER = 3 # Choose the solver, 1: APOPT, 2: BPOPT, 3: IPOPT (default)
 
-# Parameters
-# https://gekko.readthedocs.io/en/latest/quick_start.html#parameters
+# Define the model parameters
 mass_1 = 1.0                        # (kg)
 mass_2 = 1.0                        # (kg)
-c = m.Param(value=0.0)              # Spring constant (N/m/s)
-k = m.Param(value=(2*2*np.pi)**2)   # Spring constant (N/m)
+SPRING_CONSTANT = (2*2*np.pi)**2    # Spring constant (N/m)
+DAMPING_COEFF = 0.0                 # Damping coefficient (N/m/s)
 MAX_FORCE = 50.0                    # maximum force from actuator (N)
 MIN_FORCE = -50.0                   # minimum force from actuator (N)
+
+# Make the damping coefficient and spring constant parameters
+# https://gekko.readthedocs.io/en/latest/quick_start.html#parameters
+c = m.Param(value=DAMPING_COEFF)
+k = m.Param(value=SPRING_CONSTANT)
 
 # Manipulated variables - Here, it's just the force acting on the mass
 # https://gekko.readthedocs.io/en/latest/quick_start.html#manipulated-variable
@@ -66,6 +102,8 @@ x_init = 0.0
 x_dot_init = 0.0
 y_init = 0.0
 y_dot_init = 0.0
+
+x0 = [x_init, x_dot_init, y_init, y_dot_init]
 
 x = m.CV(value=x_init)
 x.STATUS = 1            # add the SP to the objective function
@@ -114,12 +152,70 @@ m.Equation(y.dt() == y_dot)
 m.Equation(mass_2 * y_dot.dt() == -k * (y - x) - c * (y_dot - x_dot))
 
 m.options.IMODE = 6 # MPC control
-m.solve(disp=True)
+m.options.SOLVER = 3
+# m.solve(disp=False)
 
 # We can get additional solution information
 # import json
 # with open(m.path+'//results.json') as f:
 #     results = json.load(f)
+
+
+# Set up NumPy arrays to allow us to plot the state and control history
+sim_time = np.linspace(0, 5, 501)
+control = np.zeros_like(sim_time)
+response = np.zeros((len(sim_time), 4))
+
+dt = sim_time[1] - sim_time[0]
+
+# Simulate the nonlinear sytem over one time interval, using the MPC solution
+# from the linear system
+for index in range(len(sim_time)-1):
+    # Now, run the nonlinear simulation. We'll use the resulting final
+    # states as the initial states in the next iteration of the MPC 
+    # solution.
+    # Pack the parameters into arrays 
+    ode_params = [mass_1, mass_2, SPRING_CONSTANT, DAMPING_COEFF, control[index]]
+    
+    start_time = time.time()
+    nonlinear_solution = solve_ivp(fun=lambda t, w: eq_of_motion_nonlinear(t, w, ode_params), 
+                                   t_span=[0, dt], 
+                                   y0=x0, 
+                                   # t_eval=np.linspace(0, dx.dt, 11),
+                                   # dense_output=False,
+                                   # max_step=max_step, 
+                                   # atol=abserr, 
+                                   # rtol=relerr
+                                   )
+
+    print('Index: {}'.format(index))                                   
+    print('ODE solution: {}'.format(time.time() - start_time))
+    
+    if not nonlinear_solution.success: 
+        # The ODE solver failed. Notify the user and print the error message
+        print('ODE solution terminated before desired final time.')
+        print('Be *very* careful trusting the results.')
+        print('Message: {}'.format(nonlinear_solution.message))
+
+    # Parse the time and response arrays from the OdeResult object
+    nonlinear_sim_time = nonlinear_solution.t
+    response[index,:] = nonlinear_solution.y[:,-1]
+    x0 = nonlinear_solution.y[:,-1]
+    
+    # retrieve measurements
+    x.MEAS = nonlinear_solution.y[0][-1]
+    x_dot.MEAS = nonlinear_solution.y[1][-1]
+    y.MEAS = nonlinear_solution.y[2][-1]
+    y_dot.MEAS = nonlinear_solution.y[3][-1]
+
+    # solve MPC
+    start_time = time.time()
+    m.solve(disp=False) # remote=False for local solve
+    print('MPC solution: {}\r\n'.format(time.time() - start_time))
+
+    control[index+1] = u.NEWVAL
+
+
 
 
 # Set the plot size - 3x2 aspect ratio is best
@@ -145,7 +241,7 @@ ax.set_axisbelow(True)
 plt.xlabel('Time (s)', fontsize=22, weight='bold', labelpad=5)
 plt.ylabel('Force (N)', fontsize=22, weight='bold', labelpad=10)
  
-plt.plot(m.time, u.value, linewidth=2, linestyle='-', label=r'$u$')
+plt.plot(sim_time, control, linewidth=2, linestyle='-', label=r'$u$')
 
 # uncomment below and set limits if needed
 # plt.xlim(0,5)
@@ -186,8 +282,8 @@ ax.set_axisbelow(True)
 plt.xlabel('Time (s)', fontsize=22, weight='bold', labelpad=5)
 plt.ylabel('Position (m)', fontsize=22, weight='bold', labelpad=10)
  
-plt.plot(m.time, x.value, linewidth=2, linestyle='-', label=r'$m_1$')
-plt.plot(m.time, y.value, linewidth=2, linestyle='--', label=r'$m_2$')
+plt.plot(sim_time, response[:,0], linewidth=2, linestyle='-', label=r'$m_1$')
+plt.plot(sim_time, response[:,2], linewidth=2, linestyle='--', label=r'$m_2$')
 
 # uncomment below and set limits if needed
 # plt.xlim(0,5)
@@ -195,7 +291,7 @@ if np.max((x.value, y.value)) < 1.25:
     plt.ylim(0, 1.25)
 
 # Create the legend, then fix the fontsize
-leg = plt.legend(loc='upper right', ncol = 2, fancybox=True)
+leg = plt.legend(loc='upper right', ncol=2, fancybox=True)
 ltext  = leg.get_texts()
 plt.setp(ltext,fontsize=18)
 
