8c71df7504d1bf27e62c8b30b6aed93b680d0e3e
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2019-08-21 11:50:59 -0400

    Updates to MPC with Gekko explaining many more solution options

--- Model Predictive Control/mpc_mass_spring_damper_Gekko.py
+++ Model Predictive Control/mpc_mass_spring_damper_Gekko.py
@@ -31,28 +31,70 @@ import matplotlib.pyplot as plt
 
 from gekko import GEKKO
 
+# Create an instance of GEKKO. By default, this points to a server at
+#  http://byu.apmonitor.com, where the solution is actually done. That means
+#  that you'll need to be connected to the Internet for it to work.
 m = GEKKO()
+
+# Optionally, you can instantiate with remote=False to solve locally. The major
+#  downside of this for most of what we do is that the local implementation 
+#  does not include IPOPT solver, which is generally significantly better than 
+#  APOPT for the types of problems we solve.
+# m = GEKKO(remote=False)
+
 m.time = np.linspace(0, 5, 501)
 
-# m.options.CTRL_HOR = 100
-# m.options.PRED_HOR = 200
-# m.options.SOLVER = 3 # Choose the solver, 1: APOPT, 2: BPOPT, 3: IPOPT (default)
+# m.options.SOLVER = 3      # Choose the solver, 1: APOPT, 2: BPOPT, 3: IPOPT (default)
+# m.options.MAX_ITER = 250  # Set the maximum number of solver iterations. Default is 250.
+# m.options.COLDSTART = 2
+# m.options.AUTO_COLD = 5
+# m.options.REDUCE = 10
 
-# Parameters
-# https://gekko.readthedocs.io/en/latest/quick_start.html#parameters
+# Parameters and Constants
+#  https://gekko.readthedocs.io/en/latest/quick_start.html#parameters
+#  https://gekko.readthedocs.io/en/latest/quick_start.html#constants
 mass_1 = 1.0                        # (kg)
 mass_2 = 1.0                        # (kg)
-c = m.Param(value=0.0)              # Spring constant (N/m/s)
-k = m.Param(value=(2*2*np.pi)**2)   # Spring constant (N/m)
+c = m.Const(value=0.0)              # Spring constant (N/m/s)
+k = m.Const(value=(2*2*np.pi)**2)   # Spring constant (N/m)
 MAX_FORCE = 50.0                    # maximum force from actuator (N)
 MIN_FORCE = -50.0                   # minimum force from actuator (N)
 
 # Manipulated variables - Here, it's just the force acting on the mass
-# https://gekko.readthedocs.io/en/latest/quick_start.html#manipulated-variable
+#  https://gekko.readthedocs.io/en/latest/quick_start.html#manipulated-variable
 u = m.MV(value=0, lb=MIN_FORCE, ub=MAX_FORCE)
 u.STATUS = 1             # allow optimizer to change this variable
-u.COST = 1e-6            # weight on control input - https://gekko.readthedocs.io/en/latest/tuning_params.html#cost
-#u.DCOST = 1e-3           # weight on rate-of-change of control input - https://gekko.readthedocs.io/en/latest/tuning_params.html#dcost
+
+# Weight on control input 
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#cost
+u.COST = 1e-6            
+
+# In general, we'll need to set the _.COST value. 
+# The others below are optional
+
+# Weight on rate-of-change of control input 
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#dcost
+# u.DCOST = 1e-1           
+
+# Explicit limit on how much the control effort can change for each iteration 
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#dmax
+#u.DMAX = 2
+
+# Explicit limit on how much the control effort can change positively 
+#  for each iteration. Overrides _.DMAX setting.
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#dmaxhi
+# u.DMAXHI = 1
+
+# Explicit limit on how much the control effort can change negatively 
+#  for each iteration. It will generally be negative to allow control effort
+#  to decrease. Overrides _.DMAX setting.
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#dmaxlo
+# u.DMAXLO = -50
+
+# Explicit limit on how much the control effort can change for each iteration - https://gekko.readthedocs.io/en/latest/tuning_params.html#dmax# Only let the control effort change every MV_STEP_HOR steps, 0=use global setting 
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#mv-step-hor
+# u.MV_STEP_HOR = 10
+
 
 
 # Controlled Variables 
@@ -88,6 +130,13 @@ x_dot.SP = 0.0          # x velocity set point (desired final state)
 y.SP = 1.0              # y position set point (desired final state)
 y_dot.SP = 0.0          # y velocity set point (desired final state)
 
+# We can also manipulate the rate of change of those setpoints. Generally, it
+#  will probably be preferable to control the setpoint manually instead of 
+#  relying on these, especially for our research purposes.
+#  https://gekko.readthedocs.io/en/latest/tuning_params.html#tau
+# x.TAU = 0.01      # time constant of trajectory
+# y.TAU = 0.01      # time constant of trajectory
+
 # Set the costs for each state 
 # https://gekko.readthedocs.io/en/latest/tuning_params.html#wsp
 x.WSP = 1.0             # Cost on x state
@@ -96,17 +145,6 @@ y.WSP = 1.0             # Cost on y state
 y_dot.WSP = 1.0e-1      # Cost on y_dot state
 
 
-# We can also manipulate the rate of change of those setpoints. Generally, it
-# will probably be preferable to control the setpoint manually instead of 
-# relying on these, especially for our research purposes.
-# x.TR_INIT = 1  # set point trajectory
-# x_dot.TR_INIT = 1  # set point trajectory
-# y.TR_INIT = 1  # set point trajectory
-# y_dot.TR_INIT = 1  # set point trajectory
-
-# x.TAU = 0.01      # time constant of trajectory
-# y.TAU = 0.01      # time constant of trajectory
-
 # Define the differential equations that define the system dynamics
 m.Equation(x.dt() == x_dot)
 m.Equation(mass_1 * x_dot.dt() == k * (y - x) + c * (y_dot - x_dot) + u)
@@ -114,7 +152,8 @@ m.Equation(y.dt() == y_dot)
 m.Equation(mass_2 * y_dot.dt() == -k * (y - x) - c * (y_dot - x_dot))
 
 m.options.IMODE = 6 # MPC control
-m.solve(disp=True)
+#m.solve(disp=False, GUI=False)  # Cold start
+m.solve(disp=True)              # Now full solution
 
 # We can get additional solution information
 # import json
